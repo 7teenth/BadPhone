0b2035badbaf17b94ce9417a5017f9becee52de5
SERKONOS <nikitakarvatskyi@gmail.com>
2025-01-19 00:17:29 +0200

    Initial commit

--- my-desktop-app/my-desktop-app/my-electron-app/BadPhone/main.js
+++ my-desktop-app/my-desktop-app/my-electron-app/BadPhone/main.js
@@ -4,8 +4,7 @@ let mainWindow;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        fullscreen: true, // Включает полноэкранный режим
         webPreferences: {
             nodeIntegration: true, // Для использования Node.js внутри приложения
         },
