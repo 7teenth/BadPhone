aa51d9d757c532b64a7aa1c915860d7880d2d766
Nikita Karvatskyi <135981602+7teenth@users.noreply.github.com>
2025-07-02 23:07:19 +0300

    ///

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  })
+
+  // Загружаем index.html из папки out
+  win.loadFile(path.join(__dirname, 'out', 'index.html'))
+
+  // Открой DevTools (если надо, можно закомментировать)
+  // win.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
