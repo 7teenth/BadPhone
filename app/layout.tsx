2c5582d1db0e2dfe9c5d2c2752552d70bc362545
Nikita Karvatskyi <135981602+7teenth@users.noreply.github.com>
2025-08-03 13:18:05 +0300

    ///

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { AppProvider } from "./context/app-context"
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin", "cyrillic"] })
 
